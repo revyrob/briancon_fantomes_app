6e45ba8bc7cf84cf6c0db593438f5c4b35f1f742
revyrob <kayle.robson@gmail.com>
2023-02-26 21:46:11 -0800

    working on position of slider

--- lib/screens/details.dart
+++ lib/screens/details.dart
@@ -16,6 +16,42 @@ class DetailsScreen extends StatefulWidget {
 class _DetailsScreenState extends State<DetailsScreen> {
   //add audio player
   final player = AudioPlayer();
+  //add for the audio player so we can add slider and info
+  bool isPlaying = false;
+  //variable for duration
+  Duration duration = Duration.zero;
+  //variable for position
+  Duration position = Duration.zero;
+  //format time function
+  String formatTime(int seconds) {
+    return '${(Duration(seconds: seconds))}'.split('.')[0].padLeft(8, '0');
+  }
+
+  //initiate state for position
+  //**currently this is making the slider work?? */
+  @override
+  void initState() {
+    super.initState();
+
+    player.onPlayerStateChanged.listen((state) {
+      setState(() {
+        isPlaying = state == PlayerState.playing;
+      });
+    });
+
+    player.onDurationChanged.listen((newDuration) {
+      setState(() {
+        duration = newDuration;
+      });
+    });
+
+    player.onPositionChanged.listen((newPosition) {
+      setState(() {
+        duration = newPosition;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,12 +106,6 @@ class _DetailsScreenState extends State<DetailsScreen> {
                 height: 25,
               ),
 
-              //Time lapse
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: const [Text("0'00"), Text("4'22")],
-              ),
-
               //buttons for play and pause
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 24.0),
@@ -84,82 +114,43 @@ class _DetailsScreenState extends State<DetailsScreen> {
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        // Expanded(
-                        //     child: NeuBox(
-
-                        //         child: Icon(Icons.play_arrow, size: 32))),
                         ElevatedButton(
-                          onPressed: () async {
-                            await player.play(AssetSource(widget.data.audio));
-                          },
-                          child: const Text('to play click here'),
-                        ),
-                        // onPressed: () {
-                        //   played.play(
-                        //       AssetSource('audio/audioAng/Chpt1.mp3'));
-                        // },
-                        // child: const Icon(Icons.play_arrow))
+                            onPressed: () async {
+                              await player.play(AssetSource(widget.data.audio));
+                            },
+                            child: const Icon(Icons.play_arrow)),
+                        ElevatedButton(
+                            onPressed: () {
+                              player.pause();
+                            },
+                            child: const Icon(Icons.pause)),
                       ],
                     )),
-              )
+              ),
+
+              //Time lapse
+              Slider(
+                min: 0,
+                max: duration.inSeconds.toDouble(),
+                value: position.inSeconds.toDouble(),
+                onChanged: (value) {
+                  final position = Duration(seconds: value.toInt());
+                  player.seek(position);
+                  player.resume();
+                },
+              ),
+
+              //for the time stamp
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                //use format time function add fill in the beginning and ending time
+                children: [
+                  Text(formatTime((duration - position).inSeconds)),
+                  Text(formatTime(position.inSeconds)),
+                ],
+              ),
             ],
           ),
-        ))
-        //Column(
-        //Image
-        //chapter and title
-        //info for playing using audio player
-        //play button and pause button for audio player
-        // children: <Widget>[
-        //   Padding(
-        //     padding: const EdgeInsets.only(bottom: 20),
-        //     child: Text(
-        //       widget.data.title,
-        //       style: const TextStyle(
-        //           color: Colors.black87,
-        //           fontSize: 25,
-        //           fontWeight: FontWeight.bold),
-        //     ),
-        //   ),
-        //   Expanded(
-        //       flex: 3,
-        //       child: Padding(
-        //         padding: const EdgeInsets.all(20.0),
-        //         child: Hero(
-        //           tag: widget.data.imageName,
-        //           child: Container(
-        //             decoration: BoxDecoration(
-        //                 color: Colors.white,
-        //                 borderRadius: BorderRadius.circular(30),
-        //                 image: DecorationImage(
-        //                     image: AssetImage(
-        //                       widget.data.imageName,
-        //                     ),
-        //                     fit: BoxFit.fill),
-        //                 boxShadow: const [
-        //                   BoxShadow(
-        //                       offset: Offset(0, 4),
-        //                       blurRadius: 4,
-        //                       color: Colors.black26)
-        //                 ]),
-        //           ),
-        //         ),
-        //       )),
-        //   Expanded(
-        //     flex: 1,
-        //     child: Padding(
-        //       padding: const EdgeInsets.only(bottom: 20),
-        //       child: Text(
-        //         widget.data.location,
-        //         style: const TextStyle(
-        //             color: Colors.black54,
-        //             fontSize: 24,
-        //             fontWeight: FontWeight.bold),
-        //       ),
-        //     ),
-        //   ),
-        // ],
-        //),
-        );
+        )));
   }
 }
